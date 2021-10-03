82d879f07b05931d612f118246a81ab07515b5c3
MoldyGH <moldydev@gmail.com>
2021-10-03 10:44:40 -0700

    ??????????????????????????????????????????????????????????

---
+++ source/CoolSystemStuff.hx
@@ -0,0 +1,29 @@
+package;
+
+// crazy system shit!!!!! (only for windows)
+#if windows
+import sys.io.File;
+import sys.io.Process;
+import haxe.io.Bytes;
+
+class CoolSystemStuff
+{
+	public static function getUsername():String
+	{
+		// uhh this one is self explanatory
+		return Sys.getEnv("USERNAME");
+	}
+
+	public static function getUserPath():String
+	{
+		// this one is also self explantory
+		return Sys.getEnv("USERPROFILE");
+	}
+
+	public static function getTempPath():String
+	{
+		// gets appdata temp folder lol
+		return Sys.getEnv("TEMP");
+	}
+}
+#end
